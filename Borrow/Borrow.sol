bef1247a6496e20704febae37d82027209b5510f
Abhijit Roy <dalvath3700@gmail.com>
2021-07-02 14:15:51 +0530

    borrow contract updated

--- Borrow/Borrow.sol
+++ Borrow/Borrow.sol
@@ -1,17 +1,31 @@
+/**
+ * Created on 2021-07-01 12:56
+ * @summary: 
+ * @author: abhijit
+ */
 pragma solidity ^0.8.0
 
+/**
+ * @title: 
+ */
 contract Borrow {
     address owner;
     uint256 fund;          // storage
     mapping(address => uint256) public amountBorrowed;          // storage
 
-
+    /*
+    	check if automatic initialization happens
+    */
     constructor() public {
         owner = msg.sender;
         fund = 0;
     }
 
 
+    /**
+     * @dev: 
+     * @param qty
+     */
     function depositFund(uint256 qty) public payable {
         // can only accessed by contract owner
         require(owner == msg.sender, "fund can only be deposited by the contract owner.");
@@ -19,8 +33,14 @@ contract Borrow {
         // update the fund balance in storage
         fund += qty;
 
+
     }
 
+    /**
+     * @dev: 
+     * @param borrower
+     * @param qty
+     */
     function borrowMoney(address borrower, uint256 qty) public {
         // permission by borrower
         require(msg.sender == borrower, "permission only granted by the borrower itself.");
@@ -34,10 +54,14 @@ contract Borrow {
         // reduce the fund balance by borrowed amount
         amountBorrowed[borrower] += qty;
 
-        // transfer the quantity to the borrower address
-        // using event
+        /*
+		transfer the quantity to the borrower address
+        using event
+        */
 
     }
+
+
 
 
 
